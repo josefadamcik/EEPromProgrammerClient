b70ad896a0a21c56d3d2418f7ad3b74bc9b3cf61
Josef Adamcik <josef.adamcik@gmail.com>
2018-10-19 11:39:39 +0200

    List serial ports

--- main.cpp
+++ main.cpp
@@ -1,30 +1,44 @@
-#include <iostream>
-#include <boost/program_options.hpp>
 #include <serial/serial.h>
+#include <boost/program_options.hpp>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 namespace po = boost::program_options;
+
+void enumerate_ports() {
+    vector<serial::PortInfo> devices_found = serial::list_ports();
+
+    vector<serial::PortInfo>::iterator iter = devices_found.begin();
+
+    while (iter != devices_found.end()) {
+        serial::PortInfo device = *iter++;
+
+        printf("(%s, %s, %s)\n", device.port.c_str(),
+               device.description.c_str(), device.hardware_id.c_str());
+    }
+}
 
 int main(int argc, char *argv[]) {
     po::options_description desc("Allowed options");
     desc.add_options()
         ("help", "produce help message")
-        ("compression", po::value<int>(), "set compression level")
-    ;
+        ("list", "list serial interfaces");
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);    
+    po::notify(vm);
 
     if (vm.count("help")) {
-        cout << desc << "\n";
+        cout << desc << endl;
         return 1;
     }
 
-    if (vm.count("compression")) {
-        cout << "Compression level was set to " 
-    << vm["compression"].as<int>() << ".\n";
-    } else {
-        cout << "Compression level was not set.\n";
+    if (vm.count("list")) {
+        cout << "Listing serial interfaces:" << endl;
+        enumerate_ports();
+        return 0;
     }
+
+    cout << desc << endl;
 }
